3c6134848dd466651fd60755bc464172ecc5c7d1
Aaron Puffer <blackfugu@users.noreply.github.com>
2020-02-02 12:13:52 -0500

    Add Business Folder

---
+++ Business/Managers.Tests/Managers.Tests/CustomerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AaronPuffer.DependencyInjection.Business.Managers;
+using AaronPuffer.DependencyInjection.Data.Repository.Interfaces;
+using AaronPuffer.DependencyInjection.Data.Repository.Stubs;
+using AaronPuffer.DependencyInjection.DTO; 
+
+namespace AaronPuffer.DependencyInjection.Business.Managers.Tests
+{
+    [TestClass]
+    public class CustomerManagerTests
+    {
+        private CustomerManager customerManager;
+
+        public CustomerManagerTests()
+        {
+            ICustomerData customerData = new CustomerDataStub();
+            customerManager = new CustomerManager(customerData);
+        }
+
+        [TestMethod]
+        public void CreateCustomer()
+        {
+            Customer customer = new Customer()
+            {
+                Address1 = "321 Test Address",
+                Address2 = "",
+                City = "West Point",
+                State = "New York",
+                FirstName = "Karen",
+                LastName = "Washington",
+                ZipCode = "33021",
+                Id = 999
+            };
+
+            customerManager.CreateCustomer(customer);
+
+            Customer addedCustomer = customerManager.GetCustomer(999);
+
+            Assert.IsNotNull(addedCustomer);
+                        
+        }
+
+        [TestMethod]
+        public void ValidateCustomer()
+        {
+            Customer addedCustomer = customerManager.GetCustomer(1);
+
+            string expected = "Jim";
+            string actual = addedCustomer.FirstName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UpdateCustomer()
+        {
+            Customer customer = customerManager.GetCustomer(2);
+
+            customer.FirstName = "Marilyn";
+
+            customerManager.UpdateCustomer(customer);
+
+            Customer updatedCustomer = customerManager.GetCustomer(2);
+
+            var expected = "Marilyn";
+            var actual = updatedCustomer.FirstName;
+
+            Assert.AreEqual(expected, actual, $"Customer Names do not match: { expected } != { actual }");
+        }
+
+        [TestMethod]
+        public void UpdateCustomerAddress()
+        {
+            customerManager.UpdateCustomerName(1, "Susan", "Bishop");
+
+            Customer customer = customerManager.GetCustomer(1);
+
+            var expected = "Susan Bishop";
+            var actual = $"{ customer.FirstName.Trim() } {customer.LastName.Trim() }";
+
+            Assert.AreEqual(expected, actual, $"Customer names do not match: { expected } != { actual }");
+        }
+    }
+}
---
+++ Business/Managers/CustomerManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AaronPuffer.DependencyInjection.Data.Repository.Implementation;
+using AaronPuffer.DependencyInjection.Data.Repository.Interfaces;
+using AaronPuffer.DependencyInjection.Data.Repository.Stubs;
+using AaronPuffer.DependencyInjection.DTO;
+
+namespace AaronPuffer.DependencyInjection.Business.Managers
+{
+    public class CustomerManager
+    {
+        private ICustomerData data;
+
+        public CustomerManager()
+        {
+            data = new CustomerData();
+        }
+
+        public CustomerManager(ICustomerData data)
+        {
+            this.data = data;
+        }
+
+        public void CreateCustomer(Customer Customer)
+        {
+            data.SaveCustomer(Customer);
+        }
+
+        public void UpdateCustomer(Customer Customer)
+        {
+            data.UpdateCustomer(Customer);
+        }
+
+        public void UpdateCustomerAddress(int CustomerId, string Address1, string Address2)
+        {
+            var Customer = data.GetCustomer(CustomerId);
+
+            Customer.Address1 = Address1;
+            Customer.Address2 = Address2;
+
+            data.UpdateCustomer(Customer);
+        }
+
+        public Customer GetCustomer(int CustomerId)
+        {
+            return data.GetCustomer(CustomerId);
+        }
+
+        public void UpdateCustomerName(int CustomerId, string FirstName, string LastName)
+        {
+            var Customer = data.GetCustomer(CustomerId);
+
+            if ((FirstName == null || FirstName == String.Empty) || (LastName == null || LastName == String.Empty))
+            {
+                throw new Exception($"Customer Information is not complete: FirstName: {FirstName}, LastName: {LastName}");
+            }
+
+            Customer.FirstName = FirstName;
+            Customer.LastName = LastName;
+
+            data.UpdateCustomer(Customer);
+        }
+    }
+}
